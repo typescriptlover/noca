31663e3257252fd3335dd80b3c34861c10a51130
guy <devtyl3r@gmail.com>
2022-10-04 18:19:08 -0400

    feat&fix: note tools, delete, animation

--- src/components/Canvas/Notes/Note.tsx
+++ src/components/Canvas/Notes/Note.tsx
@@ -6,13 +6,16 @@ import * as state from '@/lib/state';
 import { INote } from '@/types/interfaces';
 import getNoteCoords from '@/lib/getNoteCoords';
 import useJump from '@/hooks/useJump';
+import Tooltip from '@/components/ui/Tooltip';
 
 interface Props {
    note: INote;
    setNote: (id: number, newNote: INote) => void;
+   deleteNote: (id: number) => void;
 }
 
-const Note: FC<Props> = ({ note, setNote }) => {
+// TODO: don't unmount location pin on scale, performance boost
+const Note: FC<Props> = ({ note, setNote, deleteNote }) => {
    const [canvas, setCanvas] = useAtom(state.canvas);
    const [_, setBusy] = useAtom(state.busy);
    const [jumping, setJumping] = useAtom(state.jumping);
@@ -64,13 +67,13 @@ const Note: FC<Props> = ({ note, setNote }) => {
                      exit={{ opacity: 0, scale: 0.5, y: 15 }}
                      transition={{
                         ease: 'easeInOut',
-                        duration: 0.4,
+                        duration: 0.3,
                      }}
                   >
                      <div className="absolute inset-x-0 top-0 flex items-center justify-center transition-all duration-200 ease-in-out">
                         <button
                            onClick={() => jump()}
-                           className="transition duration-200 ease-linear focus:outline-none text-zinc-400 hover:text-white"
+                           className="transition-all duration-200 ease-linear outline-none text-zinc-400 hover:text-white"
                            style={{
                               marginTop: `-${locationTop()}px`,
                               fontSize: `${locationSize()}px`,
@@ -82,7 +85,31 @@ const Note: FC<Props> = ({ note, setNote }) => {
                   </motion.div>
                )}
             </AnimatePresence>
-            <div className="w-full h-full shadow-2xl focus-within:scale-110 will-change transition duration-200 ease-in-out rounded-xl border-[4px] border-base-850 focus-within:border-base-800">
+            <div className="relative w-full h-full shadow-2xl focus-within:scale-110 will-change transition duration-200 ease-in-out rounded-xl border-[4px] border-base-850 focus-within:border-base-800">
+               <motion.div
+                  initial={{ opacity: 0, y: 15 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{
+                     ease: 'easeInOut',
+                     duration: 0.3,
+                  }}
+               >
+                  <div className="absolute top-0 right-0 -mr-1 -mt-14">
+                     <div className="flex items-center rounded-xl bg-base-900">
+                        <Tooltip text="Delete">
+                           <button
+                              onClick={() => deleteNote(note.id)}
+                              className="px-3 py-2 transition duration-200 ease-linear text-zinc-400 hover:text-white"
+                           >
+                              <i className="fa-solid fa-fw fa-trash"></i>
+                           </button>
+                        </Tooltip>
+                        <button className="px-3 py-2 text-zinc-400 cursor-grab">
+                           <i className="fa-regular fa-fw fa-up-down-left-right"></i>
+                        </button>
+                     </div>
+                  </div>
+               </motion.div>
                <textarea
                   disabled={jumping}
                   onFocus={() => setBusy(true)}
@@ -95,7 +122,7 @@ const Note: FC<Props> = ({ note, setNote }) => {
                         note: e.target.value,
                      });
                   }}
-                  className="w-full h-full p-3 overflow-hidden text-base font-medium text-white rounded-lg resize-none select-text focus:overflow-auto focus:outline-none bg-base-400/10"
+                  className="w-full h-full p-3 overflow-hidden text-sm font-medium text-white rounded-lg resize-none select-text focus:overflow-auto focus:outline-none bg-base-400/10"
                />
             </div>
          </div>
--- src/components/Canvas/Notes/Notes.tsx
+++ src/components/Canvas/Notes/Notes.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react';
 import { useAtom } from 'jotai';
+import { AnimatePresence, motion } from 'framer-motion';
 
 import * as state from '@/lib/state';
 import Note from './Note';
 
 import { INote } from '@/types/interfaces';
 
+// TODO: only render notes visible in render distance
 const Notes: FC = () => {
    const [notes, setNotes] = useAtom(state.notes);
 
@@ -18,15 +20,33 @@ const Notes: FC = () => {
       );
    }
 
+   function deleteNote(id: number) {
+      const newNotes = notes.filter((n) => n.id !== id);
+      setNotes(newNotes);
+   }
+
    return (
       <div
          onMouseDown={(e: any) => e.stopPropagation()}
          id="notes"
          className="absolute"
       >
-         {notes.map((note) => (
-            <Note key={note.id} note={note} setNote={setNote} />
-         ))}
+         <AnimatePresence>
+            {notes.map((note) => (
+               <motion.div
+                  key={note.id}
+                  initial={false}
+                  animate={false}
+                  exit={{ opacity: 0, y: -25 }}
+                  transition={{
+                     ease: 'easeInOut',
+                     duration: 0.3,
+                  }}
+               >
+                  <Note note={note} setNote={setNote} deleteNote={deleteNote} />
+               </motion.div>
+            ))}
+         </AnimatePresence>
       </div>
    );
 };
