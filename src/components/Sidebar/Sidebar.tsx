d343cf2c19605834793e48a0ed8c39e15e3b95c2
guy <devtyl3r@gmail.com>
2022-10-04 18:18:41 -0400

    fix: issue

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -11,8 +11,8 @@ interface Props {
 
 const Sidebar: FC<Props> = ({ container, canvas }) => {
    return (
-      <div className="absolute z-50 inset-y-0 left-[20px] flex items-center">
-         <div className="flex flex-col px-1.5 py-2 gap-y-1 drop-shadow-2xl bg-base-900 border border-base-850 rounded-xl">
+      <div className="absolute z-50 pointer-events-none inset-y-0 left-[20px] flex items-center">
+         <div className="flex flex-col px-1.5 pointer-events-auto py-2 gap-y-1 drop-shadow-2xl bg-base-900 border border-base-850 rounded-xl">
             <Create container={container} canvas={canvas} />
             <Select container={container} canvas={canvas} />
             <Move container={container} canvas={canvas} />
